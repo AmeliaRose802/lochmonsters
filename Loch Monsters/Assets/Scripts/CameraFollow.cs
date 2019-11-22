f72e07fc636d46256e373dfd93c227ac52420fc7
Amelia <amelia.payne@mymail.champlain.edu>
2019-11-22 19:41:24 +0000

    Refactored client code. It is nice now

--- Loch Monsters/Assets/Scripts/CameraFollow.cs
+++ Loch Monsters/Assets/Scripts/CameraFollow.cs
@@ -5,22 +5,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     //Thanks to https://learn.unity.com/tutorial/movement-basics?projectId=5c514956edbc2a002069467c#5c7f8528edbc2a002053b711 for this script
-    public Transform player;        //Public variable to store a reference to the player game object
+    public Transform player;
 
 
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    private Vector3 offset;
 
     // Use this for initialization
     void Awake()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.position;
     }
 
-    // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.position + offset;
     }
 }
